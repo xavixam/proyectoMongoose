0949256d8d44215fe2a25ac6271887c6fab138b2
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-11-29 12:38:05 +0100

    no message

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { dbConnection } = require("./config/config")
 
 app.use(express.json())
 
+app.use("/products",require("./routes/products"))
+
 dbConnection()
 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
