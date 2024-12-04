080c4c00622e89f57a4c4767b2cb0aa17ea6e2a3
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-12-04 13:20:53 +0100

    Relaciones en Comment hechas

--- models/Post.js
+++ models/Post.js
@@ -9,6 +9,7 @@ const PostSchema = new mongoose.Schema({
 PostSchema.index({
     name: "text",
     likes: [{ type: ObjectId }],
+    commentIds: [{type: ObjectId, ref: "Post"}]
   });
   
 const Post = mongoose.model('Post', PostSchema);
